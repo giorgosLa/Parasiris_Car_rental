3b69fa9e4498ffa9cca78d269ece236b422c5879
giorgos.L <giorgoslankonakes@gmail.com>
2025-12-06 13:27:33 +0200

    prisma

--- prisma/migrations/0001_add_stripeSessionId/migration.sql
+++
@@ -1,6 +0,0 @@
--- AlterTable
-ALTER TABLE "Reservation" ADD COLUMN     "stripeSessionId" TEXT;
-
--- CreateIndex
-CREATE UNIQUE INDEX "Reservation_stripeSessionId_key" ON "Reservation"("stripeSessionId");
-
